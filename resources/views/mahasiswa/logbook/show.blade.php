550ff60d86d9f75557da08d087356b92bc863cda
gafrielAR <muhammad.gafriel@gmail.com>
2023-06-18 01:13:00 +0700

    error mahasiswa profile

--- resources/views/mahasiswa/logbook/show.blade.php
+++ resources/views/mahasiswa/logbook/show.blade.php
@@ -9,26 +9,64 @@
             <form action="{{ route('mahasiswa.logbook.update', ['id' => $logbook->id]) }}" method="POST">
                 @csrf
                 @method('PUT')
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                    <div>
+                        <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+                            Lengkap</label>
+                        <input type="text" id="nama"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Inputkan Nama Lengkap Anda !!" required>
+                    </div>
+                    <div>
+                        <label for="nim"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
+                        <input type="number" id="nim" name="nim"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Masukkan NIM Anda" required>
+                    </div>
+                    <div>
+                        <label for="jurusan"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jurusan</label>
+                        <input type="text" id="jurusan"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Input jurusan Anda Dengan Benar" required>
+                    </div>
+                    <div>
+                        <label for="tanggal"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal </label>
+                        <input type="date" id="tanggal" name="tanggal"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required>
+                    </div>
 
-                <div class="mb-4">
-                    <label for="logbook" class="block text-gray-700 font-semibold mb-2">Logbook</label>
-                    <textarea id="logbook" name="logbook" class="w-full px-4 py-2 border border-gray-300 rounded-md"
-                        required>{{ $logbook->logbook }}</textarea>
                 </div>
 
-                <div class="mb-4">
-                    <label for="Catatan" class="block text-gray-700 font-semibold mb-2">Catatan</label>
-                    <textarea id="Catatan" name="Catatan" class="w-full px-4 py-2 border border-gray-300 rounded-md"
-                        readonly disabled>{{ $logbook->Catatan }}</textarea>
+                <div class="">
                 </div>
-
-                <div class="mt-8">
-                    <button type="submit"
-                        class="px-4 py-2 bg-blue-500 text-black font-semibold rounded-md hover:bg-blue-600">Submit</button>
+                <div>
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Laporan
+                        Kegiatan</label>
+                    <textarea id="catatan" rows="4" name="catatan"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Leave a comment..."></textarea>
                 </div>
+                <div class="flex items-start mb-6">
+                    <div class="flex items-center h-5">
+                        <input id="remember" type="checkbox" value=""
+                            class="w-4 h-4 border border-gray-600 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-900 dark:focus:ring-green-600 dark:ring-offset-gray-800"
+                            required>
+                    </div>
+                    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Saya
+                        Menyatakan Bahwa
+                        Informasi Saya Benar Adanya </label>
+                </div>
+                <button type="submit"
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
 
         </div>
     </div>
 </div>
 @endsection
+
+@extends('pages.main')
--- resources/views/pages/logbook_mhs.blade.php
+++ resources/views/pages/logbook_mhs.blade.php
@@ -1,64 +1,55 @@
 @extends('pages.main')
 
 @section('content')
-        @csrf
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
-                    Lengkap</label>
-                <input type="text" id="nama"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Inputkan Nama Lengkap Anda !!" required>
-            </div>
-            <div>
-                <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
-                <input type="number" id="nim" name="nim"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Masukkan NIM Anda" required>
-            </div>
-            <div>
-                <label for="jurusan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jurusan</label>
-                <input type="text" id="jurusan"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Input jurusan Anda Dengan Benar" required>
-            </div>
-            <div>
-                <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal </label>
-                <input type="date" id="tanggal" name="tanggal"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required>
-            </div>
-            <div>
+@csrf
+<div class="grid gap-6 mb-6 md:grid-cols-2">
+    <div>
+        <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+            Lengkap</label>
+        <input type="text" id="nama"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Inputkan Nama Lengkap Anda !!" required>
+    </div>
+    <div>
+        <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
+        <input type="number" id="nim" name="nim"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Masukkan NIM Anda" required>
+    </div>
+    <div>
+        <label for="jurusan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jurusan</label>
+        <input type="text" id="jurusan"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Input jurusan Anda Dengan Benar" required>
+    </div>
+    <div>
+        <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal </label>
+        <input type="date" id="tanggal" name="tanggal"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required>
+    </div>
 
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload
-                    file</label>
-                <input 
-                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-green-50 dark:text-gray-400 focus:outline-none dark:bg-green-700 dark:border-green-600 dark:placeholder-green-400"
-                    id="file_input" type="file" name="upload">
+</div>
 
-            </div>
-
-        </div>
-
-        <div class="">
-        </div>
-        <div>
-            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Laporan
-                Kegiatan</label>
-            <textarea id="catatan" rows="4" name="catatan"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Leave a comment..."></textarea>
-        </div>
-        <div class="flex items-start mb-6">
-            <div class="flex items-center h-5">
-                <input id="remember" type="checkbox" value=""
-                    class="w-4 h-4 border border-gray-600 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-900 dark:focus:ring-green-600 dark:ring-offset-gray-800"
-                    required>
-            </div>
-            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Saya Menyatakan Bahwa
-                Informasi Saya Benar Adanya </label>
-        </div>
-        <button type="submit"
-            class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-    </form>
+<div class="">
+</div>
+<div>
+    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Laporan
+        Kegiatan</label>
+    <textarea id="catatan" rows="4" name="catatan"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Leave a comment..."></textarea>
+</div>
+<div class="flex items-start mb-6">
+    <div class="flex items-center h-5">
+        <input id="remember" type="checkbox" value=""
+            class="w-4 h-4 border border-gray-600 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-900 dark:focus:ring-green-600 dark:ring-offset-gray-800"
+            required>
+    </div>
+    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Saya Menyatakan Bahwa
+        Informasi Saya Benar Adanya </label>
+</div>
+<button type="submit"
+    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
 @endsection
